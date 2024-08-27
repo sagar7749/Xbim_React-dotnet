e9dfda3375807605c025c828809b099018d0f5e8
sagar-dignizant <sagar.dignizant@gmail.com>
2024-08-27 15:36:49 +0530

    remove frontend code add backend

--- obj/Debug/net8.0/CreateWexBIMApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/CreateWexBIMApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CreateWexBIMApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e3e812fbd0d787a74cfc2ae165259b65893f3d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("CreateWexBIMApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CreateWexBIMApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
